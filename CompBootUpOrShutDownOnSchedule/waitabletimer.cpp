b115d8fb1c867232577c05a63e37b4ff4204da6c
Аббас Гусенов <gusenov@live.ru>
2018-12-14 22:23:15 +0600

    Написал больше комментариев в файле waitabletimer.cpp.

--- CompBootUpOrShutDownOnSchedule/waitabletimer.cpp
+++ CompBootUpOrShutDownOnSchedule/waitabletimer.cpp
@@ -9,33 +9,46 @@ BOOL WINAPI WaitableTimer::_EnableShutDownPriv()
     // Текущий процесс:
     HANDLE process = GetCurrentProcess();
 
-
+    // Маркер доступа:
     HANDLE token;
 
+    // Открыть маркер доступа ассоциированный с процессом:
     if (!OpenProcessToken(process, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token))
-        return FALSE;
+        return FALSE;  // возвращаем ЛОЖЬ, если не удалось получить маркер доступа.
 
+    // 64-битное значение, которое гарантированно уникально только на системе,
+    // где оно сгенерировано:
     LUID luid;
+
+    // Получить LUID для привелегии выключать компьютер:
     LookupPrivilegeValue(nullptr, L"SeShutdownPrivilege", &luid);
 
+    // Настройка привелегий:
     TOKEN_PRIVILEGES priv;
-    priv.PrivilegeCount = 1;
-    priv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-    priv.Privileges[0].Luid = luid;
+    priv.PrivilegeCount = 1;  // количество привелегий.
+    priv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;  // включаем привелегию.
+    priv.Privileges[0].Luid = luid;  // указываем LUID привелегии.
 
+    // Включаем привелегию для маркера доступа:
     ret = AdjustTokenPrivileges(token, FALSE, &priv, 0, nullptr, nullptr);
 
+    // Закрываем маркер доступа:
     CloseHandle(token);
 
+    // Возвращаем ИСТИНА, если всё хорошо:
     return TRUE;
 }
 
 // Функция, которая выполнится, когда сработает таймер:
 DWORD CALLBACK ShutDownProc(LPVOID p)
 {
+    // Указатель на дескриптор:
     PHANDLE timer = (PHANDLE)p;
 
+    // Ждём доступа к таймеру:
     WaitForSingleObject(*timer, INFINITE);
+
+    // Закрываем таймер:
     CloseHandle(*timer);
 
     // Включить монитор:
@@ -53,15 +66,27 @@ BOOL WINAPI WaitableTimer::HibernateAndReboot(int secs)
 
     // Создание таймера пробуждения:
     HANDLE timer = CreateWaitableTimer(nullptr, TRUE, L"MyWaitableTimer");
+
+    // Если получить таймер не удалось, то возвращаем ЛОЖЬ:
     if (timer == nullptr)
         return FALSE;
+
+    // Таймер срабатывает каждые 100 наносекунд.
 
     __int64 nanoSecs;
     LARGE_INTEGER due;
 
+    // Указываем секунды в отрицательном виде, чтобы задать время относительно
+    // момента старта таймера:
     nanoSecs = -secs * 1000 * 1000 * 10;
-    due.LowPart = (DWORD) (nanoSecs & 0xFFFFFFFF);
-    due.HighPart = (LONG) (nanoSecs >> 32);
+
+    // Указываем время в специальной структуре типа FILETIME:
+    due.LowPart = (DWORD) (nanoSecs & 0xFFFFFFFF);  // нижняя часть файлового времени.
+    due.HighPart = (LONG) (nanoSecs >> 32);  // старшая часть файлового времени.
+
+    // Структура FILETIME - 64-битовое значение,
+    // представляющее число интервалов по 100 наносекунд с 1 января 1601
+    // (универсальное глобальное время (UTC)).
 
     // Установка таймера пробуждения:
     if (!SetWaitableTimer(timer, &due, 0, 0, 0, TRUE))
@@ -71,13 +96,17 @@ BOOL WINAPI WaitableTimer::HibernateAndReboot(int secs)
     if (GetLastError() == ERROR_NOT_SUPPORTED)
         return FALSE;
 
+    // Создаем поток:
     HANDLE thread = CreateThread(nullptr, 0, ShutDownProc, &timer, 0, nullptr);
+
+    // Если поток создать не удалось:
     if (!thread)
     {
-        CloseHandle(timer);
-        return FALSE;
+        CloseHandle(timer);  // закрываем таймер:
+        return FALSE;  // возвращаем ЛОЖЬ.
     }
 
+    // Закрываем поток:
     CloseHandle(thread);
 
     // Выключение компьютера:
